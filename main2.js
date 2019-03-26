e394fc3ae9c56bbd7feb5948a8e24d271393cb15
igorench <iturovskij5@gmail.com>
2019-03-26 11:36:01 +0200

    addFood function

--- main2.js
+++ main2.js
@@ -3,11 +3,11 @@
 function Machine(power) {
     this._enabled = false;
 
-    this.enable = function() {
+    this.enable = function () {
         this._enabled = true;
     };
 
-    this.disable = function() {
+    this.disable = function () {
         this._enabled = false;
     };
 }
@@ -16,20 +16,28 @@ function Machine(power) {
 function Fridge(power) {
     Machine.call(this, power);
     var food = [];
+    var MAXIMUM_FOOD = power / 100;
 
 
-    this.addFood = function(item) {
+    this.addFood = function (item) {
         if (!this._enabled) {
             throw new Error("Когда холодильник выключен вы не можете добавлять еду!");
         }
-        food = food.concat(arguments);
+
+        [].forEach.call(arguments, function(item) {
+            if (food.length >= MAXIMUM_FOOD) {
+                throw new Error("Холодильник заполнен!");
+            }
+            food.push(item);
+        });
     };
-    this.getFood = function() {
+    this.getFood = function () {
         return food;
     }
 }
 
-var fridge = new Fridge(200);
+var fridge = new Fridge(300);
 fridge.enable();
 fridge.addFood("котлета", "сыр"); // ошибка, холодильник выключен
+fridge.addFood("яблоко", "курица", "капуста");
 console.log(fridge.getFood());
